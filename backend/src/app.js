51ea36050230ebec5b280c1042c7734786d1bb8c
sumati4 <sumati3976@gmail.com>
2024-09-16 19:49:47 +0530

    frontend setup

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const cors = require('cors');
+import express from 'express';
+import cors from 'cors';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json()); // For parsing application/json
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello from Express!');
@@ -14,4 +14,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
